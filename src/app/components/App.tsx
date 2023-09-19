b0b5bb4b40fcaa9a94eeeaf1287a1e844341017f
JooHyung Park <dusskapark@gmail.com>
2023-09-20 02:00:59 +0800

    Updated

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,45 +1,159 @@
-// App.tsx
 import React from 'react';
-import { List, ListItem, ListItemText, AppBar, Toolbar, Typography, IconButton, Box } from '@mui/material';
-import CloudSyncIcon from '@mui/icons-material/CloudSync';
+import { Button, IconButton, Box } from '@mui/material';
+import { DataGrid, GridRowSelectionModel, GridToolbarContainer } from '@mui/x-data-grid';
+import { OpenInNew as OpenInNewIcon, Delete as DeleteIcon, AutoFixHigh as AutoFixHighIcon } from '@mui/icons-material';
 
 function App() {
   const [rows, setRows] = React.useState([]);
+  const [rowSelectionModel, setRowSelectionModel] = React.useState<GridRowSelectionModel>([]);
 
   const getComponentSet = () => {
     parent.postMessage({ pluginMessage: { type: 'get-component-set' } }, '*');
   };
 
+  const generateDummyForSelectedRows = () => {
+    rowSelectionModel.forEach((nodeId, index) => {
+      setTimeout(() => {
+        parent.postMessage({ pluginMessage: { type: 'gen-dummy', nodeId } }, '*');
+      }, index * 500); // 500ms delay between each message
+    });
+  };
+
+  const CustomToolbar = () => {
+    if (rowSelectionModel.length === 0) {
+      return null;
+    }
+  
+    return (
+      <GridToolbarContainer style={{ justifyContent: 'flex-end' }}>
+        <Button
+          variant="text"
+          color="primary"
+          startIcon={<AutoFixHighIcon />}
+          onClick={generateDummyForSelectedRows}
+        >
+          Generate Dummy
+        </Button>
+      </GridToolbarContainer>
+    );
+  };
+
+  const NoRowsComponent = () => (
+    <Box display="flex" justifyContent="center" alignItems="center" height="100%">
+      <Button variant="contained" color="primary" onClick={getComponentSet}>
+        Get Component Set
+      </Button>
+    </Box>
+  );
+
+  const handleTextDummyChange = (e, id) => {
+    const newRows = rows.map((row) => {
+      if (row.id === id) {
+        return { ...row, textDummy: e.target.value };
+      }
+      return row;
+    });
+    setRows(newRows);
+  };
+
+  const columns = [
+    { field: 'path', headerName: 'Path', flex: 2 },
+    { field: 'possibleDesigns', headerName: 'Variants', width: 100 },
+    {
+      field: 'textDummy',
+      headerName: 'Dummy',
+      width: 100,
+      renderCell: (params) => {
+        const hasTextNode = params.row.textNodeCount > 0;
+        return (
+          <div>
+            <input
+              type="number"
+              min="1"
+              max="100"
+              value={params.row.textDummy || 1}
+              disabled={!hasTextNode}
+              onChange={(e) => handleTextDummyChange(e, params.row.id)}
+            />
+            {hasTextNode && ` X ${params.row.textNodeCount}`}
+          </div>
+        );
+      },
+    },
+    {
+      field: 'totalDesigns',
+      headerName: 'Total',
+      width: 100,
+      valueGetter: (params) => (params.row.possibleDesigns || 1) * (params.row.textDummy || 1) * (params.row.textNodeCount || 1),
+    },
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      sortable: false,
+      width: 100,
+      renderCell: (params) => {
+        const hasDocumentationLinks = params.row.documentationLinks.length > 0;
+        const handleDelete = () => {
+          setRows(rows.filter((row) => row.id !== params.row.id));
+        };
+
+        return (
+          <>
+            <IconButton
+              disabled={!hasDocumentationLinks}
+              onClick={() => {
+                if (hasDocumentationLinks) {
+                  window.open(params.row.documentationLinks[0], '_blank');
+                }
+              }}
+            >
+              <OpenInNewIcon />
+            </IconButton>
+            <IconButton onClick={handleDelete}>
+              <DeleteIcon />
+            </IconButton>
+          </>
+        );
+      },
+    },
+  ];
+
   React.useEffect(() => {
     window.onmessage = (event) => {
       const { type, data } = event.data.pluginMessage;
       if (type === 'component-set-data') {
-        setRows(data);
+        const updatedRows = data.map((item) => {
+          const pathElements = item.path.split('/');
+          const newPath = `${pathElements[1]}/${pathElements[pathElements.length - 1]}`;
+
+          return {
+            id: item.id,
+            path: newPath,
+            possibleDesigns: item.possibleDesigns,
+            textNodeCount: item.textNodeCount,
+            documentationLinks: item.documentationLinks,
+          };
+        });
+        setRows(updatedRows);
       }
     };
   }, []);
 
   return (
-    <div style={{ paddingBottom: '56px' }}>
-      <List>
-        {rows.map((item, index) => (
-          <ListItem key={index}>
-            <Box border={1} borderRadius={4} p={1} width="100%">
-              <ListItemText primary={item.name} secondary={`Properties: ${item.componentPropertyDefinitions ? Object.keys(item.componentPropertyDefinitions).length : 0}`} />
-            </Box>
-          </ListItem>
-        ))}
-      </List>
-      <AppBar position="fixed" color="primary" style={{ top: 'auto', bottom: 0 }}>
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Total Items: {rows.length}
-          </Typography>
-          <IconButton color="inherit" onClick={getComponentSet}>
-            <CloudSyncIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
+    <div style={{ height: 560, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        slots={{
+          noRowsOverlay: NoRowsComponent,
+          toolbar: CustomToolbar,
+        }}
+        checkboxSelection
+        onRowSelectionModelChange={(newRowSelectionModel) => {
+          setRowSelectionModel(newRowSelectionModel);
+        }}
+        rowSelectionModel={rowSelectionModel}
+      />
     </div>
   );
 }
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,25 +1,149 @@
 // controller.ts
 
-figma.showUI(__html__, { width: 360, height: 480 });
+figma.showUI(__html__, { width: 800, height: 600 });
 
 figma.ui.onmessage = async (msg) => {
   if (msg.type === 'get-component-set') {
     console.log('get-component-set');
     const nodes = figma.root.findAll((node) => node.type === 'COMPONENT_SET') as ComponentSetNode[];
 
-    
-
     const componentSetData = nodes.map((node) => {
+      const possibleDesigns = generatePropertyCombinations(node.componentPropertyDefinitions).length;
+      const textNodeCount = calculateTextNodeCount(node.componentPropertyDefinitions);
+      const documentationLinks = node.documentationLinks;
+
       return {
         id: node.id,
         name: node.name,
-        componentPropertyDefinitions: node.componentPropertyDefinitions
+        path: getNodePath(node),
+        possibleDesigns,
+        textNodeCount,
+        documentationLinks,
+        key: node.key,
+        width: node.width,
+        height: node.height,
       };
     });
 
     figma.ui.postMessage({
       type: 'component-set-data',
-      data: componentSetData
+      data: componentSetData,
     });
   }
+  // Handle 'gen-dummy' message
+  else if (msg.type === 'gen-dummy') {
+    handleGenDummy(msg.nodeId);
+
+    
+  }
 };
+
+// Recursive function to get the path of a node
+function getNodePath(node: SceneNode): string {
+  // If the parent is a PageNode, return the node's name
+  if (node.parent && node.parent.type === 'PAGE') {
+    return node.parent.name + '/' + node.name;
+  }
+  // If the parent is a SceneNode (but not a PageNode), prepend the parent's name and recurse
+  else if (node.parent && 'name' in node.parent) {
+    return getNodePath(node.parent as SceneNode) + '/' + node.name;
+  }
+  // If the node has no parent, return the node's name
+  else {
+    return node.name;
+  }
+}
+
+// Function to generate all combinations of properties for a component set
+function generatePropertyCombinations(componentPropertyDefinitions) {
+  let combinations = [{}];
+
+  for (const property in componentPropertyDefinitions) {
+    const { type, variantOptions } = componentPropertyDefinitions[property];
+    const newCombinations = [];
+
+    if (type === 'BOOLEAN') {
+      for (const prevCombination of combinations) {
+        newCombinations.push({ ...prevCombination, [property]: true });
+        newCombinations.push({ ...prevCombination, [property]: false });
+      }
+    }
+
+    if (type === 'VARIANT') {
+      for (const prevCombination of combinations) {
+        for (const option of variantOptions) {
+          newCombinations.push({ ...prevCombination, [property]: option });
+        }
+      }
+    }
+
+    if (newCombinations.length > 0) {
+      combinations = newCombinations;
+    }
+  }
+
+  return combinations;
+}
+
+// Function to calculate the number of text nodes in a component set
+function calculateTextNodeCount(componentPropertyDefinitions) {
+  let count = 0;
+
+  for (const property in componentPropertyDefinitions) {
+    const { type } = componentPropertyDefinitions[property];
+
+    if (type === 'TEXT') {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+// Function to handle 'gen-dummy' message
+function handleGenDummy(nodeId) {
+  const node = figma.getNodeById(nodeId) as ComponentSetNode;
+
+  // Get the current page
+  const currentPage = figma.currentPage;
+
+  // Get the last position from the plugin data
+  let x = Number(currentPage.getPluginData('x')) || 0;
+  let y = Number(currentPage.getPluginData('y')) || 0;
+
+  // Get the width and height of the component set
+  const { width, height } = node;
+
+  // Generate all combinations of variant properties
+  const variantCombinations = generatePropertyCombinations(node.componentPropertyDefinitions);
+
+  // Create an instance for each combination of variant properties
+  for (const combination of variantCombinations) {
+
+    // Get the ComponentNode that matches the combination of variant properties
+    const component = node.children[0] as ComponentNode;
+    const instance = component.createInstance() as InstanceNode;
+
+    // Set the variant properties for the instance
+    instance.setProperties(combination);
+
+    // Set the position of the new instance
+    instance.x = x;
+    instance.y = y;
+
+    // Add the new instance to the current page
+    currentPage.appendChild(instance);
+
+    // Update the position for the next instance
+    x += width + 10;  // Add some padding
+    if (x >= 20000) {
+      // If the row is full, start a new row
+      x = 0;
+      y += height + 10;  // Add some padding
+    }
+  }
+
+  // Save the last position to the plugin data
+  currentPage.setPluginData('x', String(x));
+  currentPage.setPluginData('y', String(y));
+}
