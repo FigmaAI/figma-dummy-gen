76a627800df4f6c0e18745905beabf6a91315d7b
JooHyung Park <dusskapark@gmail.com>
2023-09-23 02:56:45 +0800

    Update trigger

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,29 +1,156 @@
-import React from 'react';
-import { Button, IconButton, Box } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Button, IconButton, Box, TextField } from '@mui/material';
 import { DataGrid, GridRowSelectionModel, GridToolbarContainer } from '@mui/x-data-grid';
 import { OpenInNew as OpenInNewIcon, Delete as DeleteIcon, AutoFixHigh as AutoFixHighIcon } from '@mui/icons-material';
 
+interface Row {
+  id: string;
+  path: string;
+  possibleDesigns: number;
+  textNodeCount: number;
+  documentationLinks: string[];
+  textDummy?: number;
+}
+
 function App() {
-  const [rows, setRows] = React.useState([]);
-  const [rowSelectionModel, setRowSelectionModel] = React.useState<GridRowSelectionModel>([]);
+  const [rows, setRows] = useState<Row[]>([]);
+  const [rowSelectionModel, setRowSelectionModel] = useState<GridRowSelectionModel>([]);
+
+  const handleDelete = (id: string) => {
+    setRows(rows.filter((row) => row.id !== id));
+  };
+
+  const handleTextDummyChange = (e: React.ChangeEvent<HTMLInputElement>, id: string) => {
+    const newRows = rows.map((row) => {
+      if (row.id === id) {
+        return { ...row, textDummy: Number(e.target.value) };
+      }
+      return row;
+    });
+    setRows(newRows);
+  };
 
   const getComponentSet = () => {
     parent.postMessage({ pluginMessage: { type: 'get-component-set' } }, '*');
   };
 
   const generateDummyForSelectedRows = () => {
-    rowSelectionModel.forEach((nodeId, index) => {
-      setTimeout(() => {
-        parent.postMessage({ pluginMessage: { type: 'gen-dummy', nodeId } }, '*');
-      }, index * 500); // 500ms delay between each message
-    });
+    const remainingRows = [...rowSelectionModel];
+
+    const processNextRow = () => {
+      const nodeId = remainingRows.shift();
+      if (nodeId) {
+        const textDummy = rows.find(row => row.id === nodeId)?.textDummy || 1;
+        parent.postMessage({ pluginMessage: { type: 'gen-dummy', nodeId, textDummy } }, '*');
+      }
+    };
+
+    processNextRow();
   };
+
+  useEffect(() => {
+    window.onmessage = (event) => {
+      const { type, data, nodeId } = event.data.pluginMessage;
+
+      if (type === 'component-set-data') {
+        const updatedRows = data.map((item) => {
+          const pathElements = item.path.split('/');
+          const newPath = `${pathElements[1]}/${pathElements[pathElements.length - 1]}`;
+          return {
+            id: item.id,
+            path: newPath,
+            possibleDesigns: item.possibleDesigns,
+            textNodeCount: item.textNodeCount,
+            documentationLinks: item.documentationLinks,
+          };
+        });
+        setRows(updatedRows);
+      } else if (type === 'gen-dummy-done' && nodeId) {
+        handleDelete(nodeId);
+      }
+    };
+  }, [rows]);
+
+  const columns = [
+    { field: 'path', headerName: 'Path', flex: 2 },
+    { field: 'possibleDesigns', headerName: 'Variants', width: 100 },
+    {
+      field: 'plusSign',
+      headerName: '+',
+      width: 50,
+      renderCell: () => '+',
+      sortable: false,
+      filterable: false,
+      disableClickEventBubbling: true,
+    },
+    {
+      field: 'textDummy',
+      headerName: 'Dummy',
+      width: 100,
+      renderCell: (params) => {
+        const hasTextNode = params.row.textNodeCount > 0;
+        return (
+          <TextField
+            type="number"
+            size="small"
+            inputProps={{ min: 1, max: 100 }}
+            value={params.row.textDummy || 1}
+            disabled={!hasTextNode}
+            onClick={(e) => e.stopPropagation()}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleTextDummyChange(e, params.row.id)}
+
+          />
+        );
+      },
+    },
+    {
+      field: 'timesSign',
+      headerName: 'X',
+      width: 50,
+      renderCell: (params) => `X ${params.row.textNodeCount || 1}`,
+      sortable: false,
+      filterable: false,
+      disableClickEventBubbling: true,
+    },
+    {
+      field: 'totalDesigns',
+      headerName: 'Total',
+      width: 100,
+      valueGetter: (params) => (params.row.possibleDesigns || 1) * (params.row.textDummy || 1) * (params.row.textNodeCount || 1),
+    },
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      sortable: false,
+      width: 100,
+      renderCell: (params) => {
+        const hasDocumentationLinks = params.row.documentationLinks.length > 0;
+        return (
+          <>
+            <IconButton
+              disabled={!hasDocumentationLinks}
+              onClick={() => {
+                if (hasDocumentationLinks) {
+                  window.open(params.row.documentationLinks[0], '_blank');
+                }
+              }}
+            >
+              <OpenInNewIcon />
+            </IconButton>
+            <IconButton onClick={() => handleDelete(params.row.id)}>
+              <DeleteIcon />
+            </IconButton>
+          </>
+        );
+      },
+    },
+  ];
 
   const CustomToolbar = () => {
     if (rowSelectionModel.length === 0) {
       return null;
     }
-  
+
     return (
       <GridToolbarContainer style={{ justifyContent: 'flex-end' }}>
         <Button
@@ -45,99 +172,6 @@ function App() {
       </Button>
     </Box>
   );
-
-  const handleTextDummyChange = (e, id) => {
-    const newRows = rows.map((row) => {
-      if (row.id === id) {
-        return { ...row, textDummy: e.target.value };
-      }
-      return row;
-    });
-    setRows(newRows);
-  };
-
-  const columns = [
-    { field: 'path', headerName: 'Path', flex: 2 },
-    { field: 'possibleDesigns', headerName: 'Variants', width: 100 },
-    {
-      field: 'textDummy',
-      headerName: 'Dummy',
-      width: 100,
-      renderCell: (params) => {
-        const hasTextNode = params.row.textNodeCount > 0;
-        return (
-          <div>
-            <input
-              type="number"
-              min="1"
-              max="100"
-              value={params.row.textDummy || 1}
-              disabled={!hasTextNode}
-              onChange={(e) => handleTextDummyChange(e, params.row.id)}
-            />
-            {hasTextNode && ` X ${params.row.textNodeCount}`}
-          </div>
-        );
-      },
-    },
-    {
-      field: 'totalDesigns',
-      headerName: 'Total',
-      width: 100,
-      valueGetter: (params) => (params.row.possibleDesigns || 1) * (params.row.textDummy || 1) * (params.row.textNodeCount || 1),
-    },
-    {
-      field: 'actions',
-      headerName: 'Actions',
-      sortable: false,
-      width: 100,
-      renderCell: (params) => {
-        const hasDocumentationLinks = params.row.documentationLinks.length > 0;
-        const handleDelete = () => {
-          setRows(rows.filter((row) => row.id !== params.row.id));
-        };
-
-        return (
-          <>
-            <IconButton
-              disabled={!hasDocumentationLinks}
-              onClick={() => {
-                if (hasDocumentationLinks) {
-                  window.open(params.row.documentationLinks[0], '_blank');
-                }
-              }}
-            >
-              <OpenInNewIcon />
-            </IconButton>
-            <IconButton onClick={handleDelete}>
-              <DeleteIcon />
-            </IconButton>
-          </>
-        );
-      },
-    },
-  ];
-
-  React.useEffect(() => {
-    window.onmessage = (event) => {
-      const { type, data } = event.data.pluginMessage;
-      if (type === 'component-set-data') {
-        const updatedRows = data.map((item) => {
-          const pathElements = item.path.split('/');
-          const newPath = `${pathElements[1]}/${pathElements[pathElements.length - 1]}`;
-
-          return {
-            id: item.id,
-            path: newPath,
-            possibleDesigns: item.possibleDesigns,
-            textNodeCount: item.textNodeCount,
-            documentationLinks: item.documentationLinks,
-          };
-        });
-        setRows(updatedRows);
-      }
-    };
-  }, []);
 
   return (
     <div style={{ height: 560, width: '100%' }}>
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,15 +1,19 @@
 // controller.ts
+
+import Chance from 'chance';
+
 figma.showUI(__html__, { width: 800, height: 600 });
 
 figma.ui.onmessage = async (msg) => {
   if (msg.type === 'get-component-set') {
     console.log('get-component-set');
+
     const nodes = figma.root.findAll((node) => node.type === 'COMPONENT_SET') as ComponentSetNode[];
 
     const componentSetData = nodes.map((node) => {
+
       const possibleDesigns = generatePropertyCombinations(node.componentPropertyDefinitions).length;
       const textNodeCount = calculateTextNodeCount(node.componentPropertyDefinitions);
-      const documentationLinks = node.documentationLinks;
 
       return {
         id: node.id,
@@ -17,7 +21,7 @@ figma.ui.onmessage = async (msg) => {
         path: getNodePath(node),
         possibleDesigns,
         textNodeCount,
-        documentationLinks,
+        documentationLinks: node.documentationLinks,
         key: node.key,
         width: node.width,
         height: node.height,
@@ -31,9 +35,14 @@ figma.ui.onmessage = async (msg) => {
   }
   // Handle 'gen-dummy' message
   else if (msg.type === 'gen-dummy') {
-    handleGenDummy(msg.nodeId);
-
-    
+    // Excute the handleGenDummy function. If the function is async, await it. if it done, close the plugin. if it failed, show the error message.
+    try {
+      handleGenDummy(msg);
+      // figma.closePlugin();
+    } catch (error) {
+      figma.notify('Dummy data generation failed!', { error: true, timeout: 5000 });
+      console.error(error);
+    }
   }
 };
 
@@ -53,10 +62,8 @@ function getNodePath(node: SceneNode): string {
   }
 }
 
-
-
 // Function to generate all combinations of properties for a component set
-function generatePropertyCombinations(componentPropertyDefinitions) {
+function generatePropertyCombinations(componentPropertyDefinitions, textDummyCount?: number) {
   let combinations = [{}];
 
   for (const property in componentPropertyDefinitions) {
@@ -74,6 +81,21 @@ function generatePropertyCombinations(componentPropertyDefinitions) {
       for (const prevCombination of combinations) {
         for (const option of variantOptions) {
           newCombinations.push({ ...prevCombination, [property]: option });
+        }
+      }
+    }
+
+    if (type === 'TEXT' && textDummyCount) {
+      // console.log(componentPropertyDefinitions[property])
+      const defaultValue = componentPropertyDefinitions[property].defaultValue;
+      const defaultValueType = determineDefaultValueType(defaultValue);
+      const numWords = defaultValue.split(' ').length;
+      const averageWordLength = Math.round(defaultValue.length / numWords);
+      const dummyTexts = generateDummyText(defaultValueType, textDummyCount, numWords, averageWordLength);
+
+      for (const prevCombination of combinations) {
+        for (const dummyText of dummyTexts) {
+          newCombinations.push({ ...prevCombination, [property]: dummyText });
         }
       }
     }
@@ -102,7 +124,7 @@ function calculateTextNodeCount(componentPropertyDefinitions) {
 }
 
 // Function to handle 'gen-dummy' message
-function handleGenDummy(nodeId) {
+function handleGenDummy({ nodeId, textDummy }) {
   const node = figma.getNodeById(nodeId) as ComponentSetNode;
 
   // Get the current page
@@ -116,17 +138,36 @@ function handleGenDummy(nodeId) {
   const { width, height } = node;
 
   // Generate all combinations of variant properties
-  const variantCombinations = generatePropertyCombinations(node.componentPropertyDefinitions);
+  const variantCombinations = generatePropertyCombinations(node.componentPropertyDefinitions, textDummy);
+
+  console.log(variantCombinations);
 
   // Create an instance for each combination of variant properties
   for (const combination of variantCombinations) {
+    // Check if node.children array is not empty
+    if (!node.children || node.children.length === 0) {
+      console.error('Error: node.children array is empty');
+      return;
+    }
 
     // Get the ComponentNode that matches the combination of variant properties
     const component = node.children[0] as ComponentNode;
     const instance = component.createInstance() as InstanceNode;
 
-    // Set the variant properties for the instance
-    instance.setProperties(combination);
+    // If the number of combinations matches the number of child nodes, set properties without validation
+    if (variantCombinations.length === node.children.length) {
+      instance.setProperties(combination);
+    } else {
+      try {
+        // Set the variant properties for the instance
+        instance.setProperties(combination);
+      } catch (error) {
+        console.log(`Error setting properties for combination: ${node.id}`, error);
+        instance.remove(); // Remove the instance
+
+        continue; // Skip this combination and continue with the next one
+      }
+    }
 
     // Set the position of the new instance
     instance.x = x;
@@ -136,15 +177,54 @@ function handleGenDummy(nodeId) {
     currentPage.appendChild(instance);
 
     // Update the position for the next instance
-    x += width + 10;  // Add some padding
+    x += width + 10; // Add some padding
     if (x >= 20000) {
       // If the row is full, start a new row
       x = 0;
-      y += height + 10;  // Add some padding
+      y += height + 10; // Add some padding
     }
   }
 
   // Save the last position to the plugin data
   currentPage.setPluginData('x', String(x));
   currentPage.setPluginData('y', String(y));
+
+  // Send completion message
+  figma.ui.postMessage({
+    type: 'gen-dummy-done',
+    nodeId,
+  });
+}
+
+// Function to determine the type of the defaultValue
+function determineDefaultValueType(defaultValue: string): string {
+  // Use regex to determine the type
+  if (/^\d+$/.test(defaultValue)) return 'number';
+  if (/^true|false$/.test(defaultValue)) return 'boolean';
+  return 'string';
+}
+
+// Function to generate a dummy text based on the type
+function generateDummyText(type: string, count: number, numWords: number, averageWordLength: number): string[] {
+  const chance = new Chance();
+  const dummyTexts = [];
+
+  for (let i = 0; i < count; i++) {
+    switch (type) {
+      case 'number':
+        dummyTexts.push(chance.string({ length: numWords * averageWordLength, pool: '0123456789' }));
+        break;
+      case 'boolean':
+        dummyTexts.push(chance.bool().toString());
+        break;
+      default:
+        let text = '';
+        for (let j = 0; j < numWords; j++) {
+          text += chance.syllable({ syllables: averageWordLength }) + ' ';
+        }
+        dummyTexts.push(text.trim());
+    }
+  }
+
+  return dummyTexts;
 }
